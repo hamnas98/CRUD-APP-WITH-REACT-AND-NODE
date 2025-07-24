f762474e4bf10881ff7b41927e52511fcc0b105e
hamnas98 <hamnascp98@gmail.com>
2025-07-24 14:25:48 +0530

    Setup Express server and MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000
