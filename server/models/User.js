d4b7e113bd811a002cda0731cec634f960ad0774
hamnas98 <hamnascp98@gmail.com>
2025-07-25 09:19:07 +0530

    added user schema

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
+
+const userSchema = new mongoose.Schema(
+    {
+        name : {type : string, require: true},
+        email : {type : string, require : true, unique : true},
+        password : {type : string, require : true},
+        profileImage : {type: string,default : ''}
+    },
+    {timestamps: true}
+);
+
+// hashing password before saving DB
+userSchema.pre('save', async (next) => {
+    if(!this.isModified('password')) {
+        return next()
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+    next()
+});
+
+// comparing password during login
+
+userSchema.method.matchPassword = async (enteredPassword) => {
+    return await bcrypt.compare(enteredPassword,this.password)
+};
